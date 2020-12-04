3abb77d90ab6c17697650693e2fabd2b48a47eac
Ben <ben@gluo.be>
2020-12-04 14:15:21 +0100

    Fix prop?!

--- functions.php
+++ functions.php
@@ -138,7 +138,7 @@ function GetMemes(){
 
             $entities = $result->getEntities();
             foreach($entities as $entity){
-              error_log("### ".$entity->getPartitionKey().":".$entity->getRowKey().":".$entity->getProperty("NAME")->getValue());
+              error_log("### ".$entity->getPartitionKey().":".$entity->getRowKey().":".$entity->getProperty("name"));
             }
             error_log("### ".json_encode($entities));
         } else {
